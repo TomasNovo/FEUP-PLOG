e8b4c4e88df83805fdb741d40ef4f3594bca6c42
TomasNovo <tomasnovo98@gmail.com>
2018-10-16 19:39:07 +0100

    Menu with error control added
    Menu with error control added

--- display.pl
+++ display.pl
@@ -4,20 +4,31 @@ display_menu :- write('Choose the mode you want to play :'), nl,
                 write('1 - Player vs Player '), nl,
                 write('2 - Player vs Computer '), nl,
                 write('3 - Watch Computer vs Computer '), nl,
-                write('4 - Credits '), nl.
+                write('4 - Credits '), nl, nl.
 
 
-gameChoose(X) :- A = 4 -> write('Game developed by : '), nl,
-                          write('- Joao Pedro Viveiros Franco'), nl,
-                          write('- Tomas Nuno Fernandes Novo'), nl.
-                 A = 3 -> write('z').
-                 A = 2 -> write('a').
+
+gameOption1(X) :- X = 1 -> write('Option 1');
+                  X \= 1 -> gameOption2(X).
+
+gameOption2(X) :- X = 2 -> write('Option 2');
+                  X \= 2 -> gameOption3(X).
+
+gameOption3(X) :- X = 3 -> write('Option 3');
+                  X \= 3 -> gameOption4(X).
+
+gameOption4(X) :- X = 4 -> write('Game developed by : '), nl,
+                  write('- Joao Pedro Viveiros Franco'), nl,
+                  write('- Tomas Nuno Fernandes Novo'), nl;
+                  X \= 4 -> write('You have picked an invalid option !'), nl, nl,
+                  write('Please, input again !'), nl,nl,
+                  kl.
 
 kl :- display_gameStart,
       display_menu, nl,
       write('Input: '),
       read(A),
-      gameChoose(A).
+      gameOption1(A).
 
 display_line(S) :-
         S1 is S-1,
