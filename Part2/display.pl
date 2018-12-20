c71fca27f66d919fb533e49dbdc3cd78c56a8742
up201604828@FEUPSIG.FE.UP.PT <up201604828@FEUPSIG.FE.UP.PT>
2018-12-20 13:28:44 +0000

    Started on restrictions

--- Part2/display.pl
+++ Part2/display.pl
@@ -5,53 +5,31 @@
 %%%%%%%%%%%
 
 
-draw_piece_aux([H|[]]):- write(H).
 
-draw_piece([]):- write(' _ ').
-draw_piece([H|T]):-
-	draw_piece_aux([H|T]).
+draw_piece([]):- write(' _').
+draw_piece(H):- write(' '),write(H).
 
 
-print_trio([]).
-print_trio([H|T]):-
-	T \= [],
-	draw_piece(H),
-	print_trio_aux(T),
-	print_trio(T).
-
-print_trio([H|T]):-
-	T = [],
-	draw_piece(H),
-	print_trio_aux(T),
-	print_trio(T).
-
-
-print_trio_aux([]):- write('| ').
-print_trio_aux([_|_]):-write('|').
 
 print_line_aux([]):-
-	write('|'),nl,
-	write(' ----------------------------------------------'),nl.
+	write(' |'),
+	displayBar,
+	write('|').
 print_line_aux([_|_]):-
 	write(' |').
 
 
 print_line([], _).
 print_line([H|T], I):-
-	T \= [],
-	print_trio(H),
+	draw_piece(H),
 	print_line_aux(T),
 	print_line(T, I).
 
-print_line([H|T], I):-
-	T = [],
-	print_trio(H),
-	print_line_aux(T),
-	print_line(T, I).
+
 
 printBoard([], _).
 printBoard([H|T], I) :-
-	printColumnNumber(I),
+	%% printColumnNumber(I),
 	print_line(H, I),
 	I1 is I+1,
 	printBoard(T, I1).
@@ -59,7 +37,8 @@ printBoard([H|T], I) :-
 printBoard(Board):-
 	I is 0,
 	nl,nl,nl,
-	printBoard(Board, I).
+	displayBar, write('|'),
+	printBoard(Board, I), !.
 
 
 
@@ -71,11 +50,6 @@ printColumnNumber(N):-
 	N >= 10,
 	write(N),
 	write('|').
-
-% Returns the dimensions of the board
-getBoardSize([H|T], X, Y):-
-	length(H, X),
-	length([H|T], Y).
 
 
 
@@ -90,18 +64,9 @@ clear_console(N) :-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-displayBar(Board):-
-	getBoardSize(Board, W, _),
-	I is 1,
-	write('   ___'),
-	displayBar(Board, W, I).
+displayBar:-
+	nl,write(' -----------------------------------'),nl.
 
-displayBar(Board, Width, N) :-
-	write('|___'),
-	(N1 is N + 1,
-	N1 @< Width,
-	displayBar(Board, Width, N1);
-	true).
 
 getPiece([H|T], X, Y, Piece) :-
 	nth0(Y, [H|T], Line),
--- Part2/logic.pl
+++ Part2/logic.pl
@@ -1,4 +1,4 @@
-:- consult('utilities.pl'), use_module(library(lists)).
+:- consult('utilities.pl'), use_module(library(lists)), use_module(library(clpfd)).
 
 initialBoard(	[
 				 [ [], [], [], [], [], [], [], [], [] ],
@@ -12,6 +12,14 @@ initialBoard(	[
 				 [ [], [], [], [], [], [], [], [], [] ]
 				]
 			).
+
+
+initialHints([ 
+	[3,4,5],[8,9],[1,2],[1,3],[6,7],[5,6],[],[8,9],
+	[3,1],[],[5,4],[4,3],[7,5][9,8],[],[6,5,4],[2,1],
+	[3,2],[9,1],[8,7],[5,4,3],[9,8],[7,6],[9,8],[3,2],[7,6],
+	[3,4],[],[1],[2,3,4],[6,8],[],[6,7],[5,8],[]
+	] ).
 
 
 testingBoard(	[
@@ -46,13 +54,133 @@ initialSelection(X):-
 	fillList(3, Y, X).
 
 getElement(Board, X, Y, Piece) :-
-	nth0(Y, Board, Line),
-	nth0(X, Line, Piece).
+	element(Y, Board, Line),
+	element(X, Line, Piece).
+
+
+getBoardSize([H|T], Width, Height):-
+	length([H|T], Height),
+	length(H, Width).
+
+
+fillBoard(Board):-
+	length(Board, 9),
+	fillBoardHorizontal(Board),
+	fillBoardVertical(Board),
+	fillBoardBox(Board),
+
+	initialHints(Hints),
+	fillHints(Board, Hints, 36),
+
+	recursiveLabeling(Board).
+
+
+fillBoardHorizontal([]).
+fillBoardHorizontal([H|T]):-
+	length(H, 9),
+	domain(H, 1, 9),
+	all_different(H),
+	fillBoardHorizontal(T).
+
+fillBoardVertical(Board):-
+	fillBoardVertical(Board, 9).
+
+
+fillBoardVertical(_, 0).
+fillBoardVertical(Board, I):-
+
+	fillBoardVertical(Board, I, [], List),
+	domain(List, 1, 9),
+	all_different(List),
+
+	I1 is I-1,
+	fillBoardVertical(Board, I1).
+
+fillBoardVertical([], _, List, List).
+fillBoardVertical([H|T], I, List, List2):-
+	element(I,H,A),
+	fillBoardVertical(T, I, [A|List], List2).
+
+fillBoardBox(Board):-
+	fillBoardBox(Board, 9).
+
+
+fillBoardBox(_, 0).
+fillBoardBox(Board, I):-
+	I1 is I-1,
+	X is mod(I1,3)*3,
+	Y is div(I1,3)*3,
+
+	fillBoardBox(Board, X, Y, 9, [], List),
+	domain(List, 1, 9),
+	all_different(List),
+
+	fillBoardBox(Board, I1).
+
+fillBoardBox(_, _, _, 0, List, List).
+fillBoardBox(Board, X, Y, I, List, List2):-
+	I1 is I-1,
+	X1 is X+mod(I1,3)+1,
+	Y1 is Y+div(I1,3),
+
+	write(X1),nl,write(Y1),nl,nl,
+
+	nth0(Y1, Board, Row),
+	element(X1, Row, A),
+	fillBoardBox(Board, X, Y, I1, [A|List], List2).
+
+
+fillHints(Board, [], 0).
+fillHints(Board, [H|T], I):-
+	fillHint(Board, H, I),
+
+	I1 is I-1,
+	fillHints(Board, T, I1).
+
+fillHint(Board, [], _).
+fillHint(Board, [H|T], I):-
+	
+
+
+
+recursiveLabeling([]).
+recursiveLabeling([H|T]):-
+	labeling([], H),
+	recursiveLabeling(T).
+
+
+getTriplet(Board, I):-
+	A is mod(I,9),
+	B is div(I,9),
+
+	X is A,
+	Y is B,
+
+getTriplet2(Board, I, A, B, List):-
+	B = 0,
+	Y = 1,
+	getElement(Board, A, B)
+
+getTriplet2(Board, I, A, B, List):-
+	B = 1,
+	X = 9,
+	getElement(Board, A, B)
+
+getTriplet2(Board, I, A, B, List):-
+	B = 2,
+	Y = 9,
+	getElement(Board, A, B)
+
+getTriplet2(Board, I, A, B, List):-
+	B = 3,
+	X = 0,
+	getElement(Board, A, B)
+
 
 check_valid_sudoku(Board):-
 	check_valid_sudoku_vertical(Board),
-	check_valid_sudoku_horizontal(Board).
-	%% check_valid_sudoku_box(Board).
+	check_valid_sudoku_horizontal(Board),
+	check_valid_sudoku_box(Board).
 
 check_valid_sudoku_horizontal([]).
 check_valid_sudoku_horizontal([H|T]):-
